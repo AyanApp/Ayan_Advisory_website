84e47d93f5ca1bccfc7d020a5e035bea933c06da
AyanApp <itsupport@ayantech.com>
2026-05-27 15:25:05 +0530

    Contact&Navigation&Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,76 @@
+// App.tsx
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import Navigation from "./components/sections/Navigation";
 import Footer from "./components/sections/Footer";
+
 import HomeSection from "./components/sections/HomeSection";
 
+import ContactPage from "./components/pages/contact";
+
+
 function App() {
+
   return (
-    <div>
 
-      <Navigation />
-      <HomeSection /> 
-      <Footer />
+    <BrowserRouter>
 
-    </div>
+      <Routes>
+
+        {/* ================= HOME PAGE ================= */}
+
+        <Route
+          path="/"
+          element={
+            <div>
+
+              <Navigation />
+
+              <HomeSection />
+
+              <Footer />
+
+            </div>
+          }
+        />
+
+        {/* ================= CONTACT PAGE ================= */}
+
+        <Route
+          path="/contact"
+          element={<ContactPage />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+
   );
+
 }
 
 export default App;
+
+
+// import Navigation from "./components/sections/Navigation";
+// import Footer from "./components/sections/Footer";
+// import HomeSection from "./components/sections/HomeSection";
+
+// function App() {
+//   return (
+//     <div>
+
+//       <Navigation />
+//       <HomeSection /> 
+//       <Footer />
+
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -4,4 +4,6 @@ export const ENDPOINTS = {
     "/api/ad-2-home-page?populate[0]=HeroImage&populate[1]=clientLogo.logo&populate[2]=HomePage.image&populate[3]=IOS_Certificates.ISO_Logo",
   FOOTER:
     "/api/ad-3-footer?populate[0]=Ayan_Logo&populate[1]=socialLinks",
+  CONTACT_PAGE: "/api/contact-page",
+  CONTACT_SUBMIT: "/api/contact-page/send-email",
 };
